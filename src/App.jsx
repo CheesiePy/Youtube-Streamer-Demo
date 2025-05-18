e507a2f44391d31ece1beef73871aee19a949af9
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2025-05-18 14:23:26 +0300

    adding suppot for backward for webOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 // src/App.jsx
+import { useEffect } from 'react'
 import './App.css'
 import VideoSelector from './components/VideoSelector'
 import VideoPlayer   from './components/VideoPlayer'
 
 export default function App() {
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      // this is a webOS specific function to close the app
+      // it’s not part of the web standard
+      // and it’s not available in the browser
+      if (e.which === 461 && parent.webOS && typeof parent.webOS.platformBack === 'function') {
+        parent.webOS.platformBack()
+      }
+    }
+    window.addEventListener('keydown', onKeyDown)
+    return () => window.removeEventListener('keydown', onKeyDown)
+  }, [])
+
   return (
     <div className="app-container">
       <VideoSelector />
