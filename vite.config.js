d84a4eb5eff3f7ad2df220837c2c71ef2da4f1b6
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2025-05-18 12:22:32 +0300

    trying to fix problem in deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Youtube-Streamer-Demo/',
+  base: '/youtube-streamer-demo/',
   plugins: [react()],
 })
